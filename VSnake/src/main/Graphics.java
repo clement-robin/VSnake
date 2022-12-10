9031cfa22cc185adf331891134c4c5ead84747ea
Fourrep <76952150+Fourrep@users.noreply.github.com>
2022-12-09 22:16:58 -0500

    Jeux Snake Basique

---
+++ VSnake/src/main/Graphics.java
@@ -0,0 +1,156 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Graphics extends JPanel implements ActionListener {
+
+    static final int WIDTH = 800;
+    static final int HEIGHT = 800;
+    static final int TICK_SIZE = 50;
+    static final int BOARD_SIZE = (WIDTH * HEIGHT) / (TICK_SIZE * TICK_SIZE);
+
+    final Font font = new Font("TimesRoman", Font.BOLD, 30);
+
+    int[] snakePosX = new int[BOARD_SIZE];
+    int[] snakePosY = new int[BOARD_SIZE];
+    int snakeLength;
+
+    Food food;
+    int foodEaten;
+
+    String direction = "Droite";
+    boolean isMoving = false;
+    final Timer timer = new Timer(150, this);
+
+    public Graphics() {
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.WHITE);
+        this.setFocusable(true);
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (isMoving) {
+                    switch (e.getKeyCode()) {
+                        case KeyEvent.VK_LEFT:
+                            if (direction != "Droite") {
+                                direction = "Gauche";
+                            }
+                            break;
+                        case KeyEvent.VK_RIGHT:
+                            if (direction != "Gauche") {
+                                direction = "Droite";
+                            }
+                            break;
+                        case KeyEvent.VK_UP:
+                            if (direction != "Bas") {
+                                direction = "Haut";
+                            }
+                            break;
+                        case KeyEvent.VK_DOWN:
+                            if (direction != "Haut") {
+                                direction = "Bas";
+                            }
+                            break;
+                    }
+                } else {
+                    start();
+                }
+            }
+        });
+
+        start();
+    }
+
+    protected void start() {
+        snakePosX = new int[BOARD_SIZE];
+        snakePosY = new int[BOARD_SIZE];
+        snakeLength = 5;
+        foodEaten = 0;
+        direction = "Droite";
+        isMoving = true;
+        spawnFood();
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(java.awt.Graphics g) {
+        super.paintComponent(g);
+
+        if (isMoving) {
+            g.setColor(Color.BLUE);
+            g.fillOval(food.getPosX(), food.getPosY(), TICK_SIZE, TICK_SIZE);
+
+            g.setColor(Color.DARK_GRAY);
+            for (int i = 0; i < snakeLength; i++) {
+                g.fillRect(snakePosX[i], snakePosY[i], TICK_SIZE, TICK_SIZE);
+            }
+        } else {
+            String scoreText = String.format("The End... Score: %d... Press any key to play again!", foodEaten);
+            g.setColor(Color.BLACK);
+            g.setFont(font);
+            g.drawString(scoreText, (WIDTH - getFontMetrics(g.getFont()).stringWidth(scoreText)) / 2, HEIGHT / 2);
+        }
+    }
+
+    protected void move() {
+        for (int i = snakeLength; i > 0; i--) {
+            snakePosX[i] = snakePosX[i-1];
+            snakePosY[i] = snakePosY[i-1];
+        }
+
+        switch (direction) {
+            case "Haut" -> snakePosY[0] -= TICK_SIZE;
+            case "Bas" -> snakePosY[0] += TICK_SIZE;
+            case "Gauche" -> snakePosX[0] -= TICK_SIZE;
+            case "Droite" -> snakePosX[0] += TICK_SIZE;
+        }
+    }
+
+    protected void spawnFood() {
+        food = new Food();
+
+
+    }
+
+    protected void eatFood() {
+        if ((snakePosX[0] == food.getPosX()) && (snakePosY[0] == food.getPosY())) {
+            snakeLength++;
+            foodEaten++;
+            spawnFood();
+
+        }
+    }
+
+    protected void collisionTest() {
+        for (int i = snakeLength; i > 0; i--) {
+            if ((snakePosX[0] == snakePosX[i]) && (snakePosY[0] == snakePosY[i])) {
+                isMoving = false;
+                break;
+            }
+        }
+
+        if (snakePosX[0] < 0 || snakePosX[0] > WIDTH - TICK_SIZE || snakePosY[0] < 0 || snakePosY[0] > HEIGHT - TICK_SIZE) {
+            isMoving = false;
+        }
+
+        if (!isMoving) {
+            timer.stop();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (isMoving) {
+            move();
+            collisionTest();
+            eatFood();
+        }
+
+        repaint();
+    }
+}

7771d60cbf175be6768696d8ef155e10679107e1
Clément ROBIN <robin.clement78@gmail.com>
2022-12-12 17:50:15 -0500

    Off line VS

--- VSnake/src/main/Graphics.java
+++ VSnake/src/main/Graphics.java
@@ -1,64 +1,93 @@
 package main;
 
 import javax.swing.*;
-
-import Painters.Painter;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+import Painters.*;
+import Painters.Painter;
+
 public class Graphics extends JPanel implements ActionListener {
 
     static final int WIDTH = 800;
-    static final int HEIGHT = 800;
+    static final int HEIGHT = 600;
     static final int TICK_SIZE = 50;
     static final int BOARD_SIZE = (WIDTH * HEIGHT) / (TICK_SIZE * TICK_SIZE);
 
     final Font font = new Font("TimesRoman", Font.BOLD, 30);
 
-    int[] snakePosX = new int[BOARD_SIZE];
-    int[] snakePosY = new int[BOARD_SIZE];
-    int snakeLength;
+    int[] snake1PosX;
+    int[] snake1PosY;
+    int snake1Length;
+    
+    int[] snake2PosX;
+    int[] snake2PosY;
+    int snake2Length;
 
     Food food;
-    int foodEaten;
-
-    String direction = "Droite";
-    boolean isMoving = false;
-    final Timer timer = new Timer(150, this);
+    int foodEaten1;
+    int foodEaten2;
     
-    Painter p;
+    Painter painter1;
+    Painter painter2;
+
+    String direction1;
+    boolean isMoving1 = false;
+    
+    String direction2;
+    boolean isMoving2 = false;
+    final Timer timer = new Timer(500, this);
 
     public Graphics() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        this.setBackground(Color.WHITE);
+        this.setBackground(new Color(169, 215, 81));
         this.setFocusable(true);
         this.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                if (isMoving) {
+                if (isMoving1 && isMoving2) {
                     switch (e.getKeyCode()) {
                         case KeyEvent.VK_LEFT:
-                            if (direction != "Droite") {
-                                direction = "Gauche";
+                            if (direction1 != "Droite") {
+                                direction1 = "Gauche";
                             }
                             break;
                         case KeyEvent.VK_RIGHT:
-                            if (direction != "Gauche") {
-                                direction = "Droite";
+                            if (direction1 != "Gauche") {
+                                direction1 = "Droite";
                             }
                             break;
                         case KeyEvent.VK_UP:
-                            if (direction != "Bas") {
-                                direction = "Haut";
+                            if (direction1 != "Bas") {
+                                direction1 = "Haut";
                             }
                             break;
                         case KeyEvent.VK_DOWN:
-                            if (direction != "Haut") {
-                                direction = "Bas";
+                            if (direction1 != "Haut") {
+                                direction1 = "Bas";
+                            }
+                            break;
+                        case KeyEvent.VK_Q:
+                            if (direction2 != "Droite") {
+                                direction2 = "Gauche";
+                            }
+                            break;
+                        case KeyEvent.VK_D:
+                            if (direction2 != "Gauche") {
+                                direction2 = "Droite";
+                            }
+                            break;
+                        case KeyEvent.VK_Z:
+                            if (direction2 != "Bas") {
+                                direction2 = "Haut";
+                            }
+                            break;
+                        case KeyEvent.VK_S:
+                            if (direction2 != "Haut") {
+                                direction2 = "Bas";
                             }
                             break;
                     }
@@ -72,44 +101,90 @@ public class Graphics extends JPanel implements ActionListener {
     }
 
     protected void start() {
-        snakePosX = new int[BOARD_SIZE];
-        snakePosY = new int[BOARD_SIZE];
-        snakeLength = 5;
-        foodEaten = 0;
-        direction = "Droite";
-        isMoving = true;
+    	
+    	
+        snake1PosX = new int[BOARD_SIZE];      
+        snake1PosY = new int[BOARD_SIZE];
+        snake1Length = 5;
+        foodEaten1 = 0;
+        direction1 = "Droite";
+        isMoving1 = true;
+        
+        snake2PosX = new int[BOARD_SIZE];
+        snake2PosX[0] = 0;
+        
+        snake2PosY = new int[BOARD_SIZE];
+        snake2PosY[0] = (HEIGHT - TICK_SIZE) /2;
+        snake2PosY[1] = (HEIGHT - TICK_SIZE) /2;
+        snake2PosY[2] = (HEIGHT - TICK_SIZE) /2;
+        snake2PosY[3] = (HEIGHT - TICK_SIZE) /2;
+
+        
+        snake2Length = 5;
+        foodEaten2 = 0;
+        direction2 = "Droite";
+        isMoving2 = true;
+        
         spawnFood();
         timer.start();
-        p = new Painter("snake-graphics.png");
+        painter1 = new Painter("snake-graphics.png");
+        painter2 = new Painter("snake-graphics2.png");
     }
 
     @Override
     protected void paintComponent(java.awt.Graphics g) {
         super.paintComponent(g);
+        
+        g.setColor(new Color(162, 209, 73));
+        int ligne = 0;
+        for (int i = 0; i < WIDTH; i+=TICK_SIZE) {
+        	int pair = 0;
+        	if(ligne%2==0)	pair = TICK_SIZE;
+        	for (int j = pair; j < HEIGHT; j+= (TICK_SIZE*2)) {
+        		g.fillRect(i, j, TICK_SIZE, TICK_SIZE);
+        	}
+        	ligne++;
+        }
 
-        if (isMoving) {
-        	p.paintFood(g, food);
-            p.paintSnake(g, snakePosX, snakePosY, snakeLength);
+        if (isMoving1 && isMoving2) {
+        	painter1.paintFood(g, food);
+        	painter1.paintSnake(g, snake1PosX, snake1PosY, snake1Length);
+        	painter2.paintSnake(g, snake2PosX, snake2PosY, snake2Length);
+
+
         } else {
-            String scoreText = String.format("The End... Score: %d... Press any key to play again!", foodEaten);
+            String scoreText = String.format("The End... Press any key to play again!");
             g.setColor(Color.BLACK);
             g.setFont(font);
             g.drawString(scoreText, (WIDTH - getFontMetrics(g.getFont()).stringWidth(scoreText)) / 2, HEIGHT / 2);
-        }   
+        }
     }
 
     protected void move() {
-        for (int i = snakeLength; i > 0; i--) {
-            snakePosX[i] = snakePosX[i-1];
-            snakePosY[i] = snakePosY[i-1];
+        for (int i = snake1Length; i > 0; i--) {
+            snake1PosX[i] = snake1PosX[i-1];
+            snake1PosY[i] = snake1PosY[i-1];
         }
 
-        switch (direction) {
-            case "Haut" -> snakePosY[0] -= TICK_SIZE;
-            case "Bas" -> snakePosY[0] += TICK_SIZE;
-            case "Gauche" -> snakePosX[0] -= TICK_SIZE;
-            case "Droite" -> snakePosX[0] += TICK_SIZE;
+        switch (direction1) {
+            case "Haut" -> snake1PosY[0] -= TICK_SIZE;
+            case "Bas" -> snake1PosY[0] += TICK_SIZE;
+            case "Gauche" -> snake1PosX[0] -= TICK_SIZE;
+            case "Droite" -> snake1PosX[0] += TICK_SIZE;
         }
+        
+        for (int i = snake2Length; i > 0; i--) {
+            snake2PosX[i] = snake2PosX[i-1];
+            snake2PosY[i] = snake2PosY[i-1];
+        }
+        
+        switch (direction2) {
+	        case "Haut" -> snake2PosY[0] -= TICK_SIZE;
+	        case "Bas" -> snake2PosY[0] += TICK_SIZE;
+	        case "Gauche" -> snake2PosX[0] -= TICK_SIZE;
+	        case "Droite" -> snake2PosX[0] += TICK_SIZE;
+	    }
+        
     }
 
     protected void spawnFood() {
@@ -119,34 +194,56 @@ public class Graphics extends JPanel implements ActionListener {
     }
 
     protected void eatFood() {
-        if ((snakePosX[0] == food.getPosX()) && (snakePosY[0] == food.getPosY())) {
-            snakeLength++;
-            foodEaten++;
+        if ((snake1PosX[0] == food.getPosX()) && (snake1PosY[0] == food.getPosY())) {
+            snake1Length++;
+            foodEaten1++;
+            spawnFood();
+
+        }
+        
+        if ((snake2PosX[0] == food.getPosX()) && (snake2PosY[0] == food.getPosY())) {
+            snake2Length++;
+            foodEaten2++;
             spawnFood();
 
         }
     }
 
     protected void collisionTest() {
-        for (int i = snakeLength; i > 0; i--) {
-            if ((snakePosX[0] == snakePosX[i]) && (snakePosY[0] == snakePosY[i])) {
-                isMoving = false;
+        for (int i = snake1Length; i > 0; i--) {
+            if ((snake1PosX[0] == snake1PosX[i]) && (snake1PosY[0] == snake1PosY[i])) {
+                isMoving1 = false;
+                break;
+            }
+        }
+        
+        for (int i = snake2Length; i > 0; i--) {
+            if ((snake2PosX[0] == snake2PosX[i]) && (snake2PosY[0] == snake2PosY[i])) {
+                isMoving2 = false;
                 break;
             }
         }
 
-        if (snakePosX[0] < 0 || snakePosX[0] > WIDTH - TICK_SIZE || snakePosY[0] < 0 || snakePosY[0] > HEIGHT - TICK_SIZE) {
-            isMoving = false;
+        if (snake1PosX[0] < 0 || snake1PosX[0] > WIDTH - TICK_SIZE || snake1PosY[0] < 0 || snake1PosY[0] > HEIGHT - TICK_SIZE) {
+            isMoving1 = false;
+        }
+        
+        if (snake2PosX[0] < 0 || snake2PosX[0] > WIDTH - TICK_SIZE || snake2PosY[0] < 0 || snake2PosY[0] > HEIGHT - TICK_SIZE) {
+            isMoving2 = false;
         }
 
-        if (!isMoving) {
+        if (!isMoving1) {
+            timer.stop();
+        }
+        
+        if (!isMoving2) {
             timer.stop();
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (isMoving) {
+        if (isMoving1 && isMoving2) {
             move();
             collisionTest();
             eatFood();
